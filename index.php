40a8d28380a97ab512f9da2b5f9ce06551d9b0f8
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-12 22:24:31 +0300

    added registration form

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-    echo "sd"
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Регестрация</title>
+    </head>
+    <body>
+        <h2>Регестрация</h2>
+        <form>
+            Логин <input type="text"><br><br>
+            Пароль <input type="text"><br><br>
+            <input type="submit" value="Зарегестрироватиься">
+        </form>
+    </body>
+</html>
