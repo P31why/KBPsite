99e191e77668799eb6e6e01d9f0be8c92d788b1c
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-12 22:27:56 +0300

    added login form

---
+++ LoginPage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Авторизация</title>
+    </head>
+    <body>
+        <h2>Авторизация</h2>
+        <form>
+            Логин <input type="text"><br><br>
+            Пароль <input type="text"><br><br>
+            <input type="submit" value="Войти">
+        </form>
+    </body>
+</html>
