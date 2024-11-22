6286b2fa2494762dda3353f54cf69d82d4de2334
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-22 12:03:33 +0300

    added connection in login

--- LoginPage.php
+++ LoginPage.php
@@ -6,9 +6,19 @@
     <body>
         <h2>Авторизация</h2>
         <form method="POST" action="#">
-            Логин <input type="text"><br><br>
-            Пароль <input type="text"><br><br>
+            Логин <input name="loginA" type="text"><br><br>
+            Пароль <input name="passwordA" type="text"><br><br>
             <input type="submit" value="Войти">
         </form>
+        <?php
+            if(isset($_POST['loginA']) && isset($_POST['passwordA'])){
+                try{
+                    $connection=new PDO ("mysql:host=127.127.126.25;port=3306;dbName=UsersSite","root","");
+                    
+                }catch(PDOException $e){
+                    echo $e->getMessage();
+                }
+            }
+        ?>
     </body>
 </html>
