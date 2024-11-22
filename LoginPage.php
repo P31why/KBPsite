78bdac3d2cd1520782676c4d8c04d92a77a1e3fb
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-22 15:39:45 +0300

    added query for compare data in for and in db

--- LoginPage.php
+++ LoginPage.php
@@ -5,20 +5,28 @@
     </head>
     <body>
         <h2>Авторизация</h2>
-        <form method="POST" action="#">
+        <form method="POST" action="#" autocomplete="off">
             Логин <input name="loginA" type="text"><br><br>
             Пароль <input name="passwordA" type="text"><br><br>
             <input type="submit" value="Войти">
         </form>
         <?php
+        if($_SERVER['REQUEST_METHOD']=='POST'){
             if(isset($_POST['loginA']) && isset($_POST['passwordA'])){
                 try{
-                    $connection=new PDO ("mysql:host=127.127.126.25;port=3306;dbName=UsersSite","root","");
-                    
+                    $connection=new PDO ("mysql:host=127.127.126.25;port=3306;dbname=UsersSite","root","");
+                    $login=$_POST['loginA'];
+                    $password=$_POST['passwordA'];
+                    $query="SELECT * FROM UsersKBP WHERE userName='$login' and userPassword='$password'";
+                    if($connection->query($query)){
+                        echo "Вы успешно вошли!!!";
+                    }
                 }catch(PDOException $e){
                     echo $e->getMessage();
                 }
             }
+        }
+
         ?>
     </body>
 </html>
