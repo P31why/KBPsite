828aedf8921bb2b92b01139d883f4c6e066057c4
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-25 21:38:58 +0300

    added user verify

--- LoginPage.php
+++ LoginPage.php
@@ -12,19 +12,23 @@
         </form>
         <?php
         if($_SERVER['REQUEST_METHOD']=='POST'){
-            if(isset($_POST['loginA']) && isset($_POST['passwordA'])){
+            if(!empty($_POST['loginA']) && !empty($_POST['passwordA'])){
                 try{
                     $connection=new PDO ("mysql:host=127.127.126.25;port=3306;dbname=UsersSite","root","");
                     $login=$_POST['loginA'];
                     $password=$_POST['passwordA'];
-                    $query="SELECT * FROM UsersKBP WHERE userName='$login' and userPassword='$password'";
-                    if($connection->query($query)){
-                        echo "Вы успешно вошли!!!";
+                    $query=$connection->prepare('SELECT * FROM UsersKBP WHERE userName = ?');
+                    $query->execute([$login]);
+                    $user=$query->fetch(PDO::FETCH_ASSOC);
+                    if($user){
+                        echo "вы успешно вошли";
                     }
+                    else echo "такого пользователя нет";
                 }catch(PDOException $e){
                     echo $e->getMessage();
                 }
             }
+            else echo "введите данные в поля";
         }
 
         ?>
